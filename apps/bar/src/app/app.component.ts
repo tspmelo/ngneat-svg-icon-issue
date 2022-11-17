058b3edaee896767ad3949520c5cac083c34fae7
Tiago Melo <tmelo@letsgetchecked.com>
2022-11-17 10:54:39 +0000

    Initial commit

---
+++ apps/bar/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'foo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'bar';
+}
